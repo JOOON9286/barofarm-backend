5b8c4582f623809f24d5c44d61cf6aee0046c30e
minaaa <tera9131@gmail.com>
2025-04-15 11:02:30 +0900

    tes

---
+++ src/main/java/com/example/barofarm_backend/BarofarmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.barofarm_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarofarmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarofarmBackendApplication.class, args);
+	}
+
+}
