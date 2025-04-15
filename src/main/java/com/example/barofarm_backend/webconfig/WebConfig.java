7bc07beb0983fa2ac9b22eff8d5db8fb22606fbb
minaaa <tera9131@gmail.com>
2025-04-15 16:58:01 +0900

    대문자w변경

--- src/main/java/com/example/barofarm_backend/webconfig/WebConfig.java
+++ src/main/java/com/example/barofarm_backend/webconfig/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.barofarm_backend.WebConfig;
+package com.example.barofarm_backend.webconfig;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
