365a6a8188a51c6089bd3d9b1b9a5dc3b6cf215f
minaaa <tera9131@gmail.com>
2025-05-06 23:50:17 +0900

    domain -> entity 폴더명 수정

--- src/main/java/com/example/barofarm_backend/repository/CartItemRepository.java
+++ src/main/java/com/example/barofarm_backend/repository/CartItemRepository.java
@@ -1,6 +1,6 @@
 package com.example.barofarm_backend.repository;
 
-import com.example.barofarm_backend.domain.CartItem;
+import com.example.barofarm_backend.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
--- src/main/java/com/example/barofarm_backend/repository/CartRepository.java
+++ src/main/java/com/example/barofarm_backend/repository/CartRepository.java
@@ -1,7 +1,7 @@
 package com.example.barofarm_backend.repository;
 
-import com.example.barofarm_backend.domain.Cart;
-import com.example.barofarm_backend.domain.User;
+import com.example.barofarm_backend.entity.Cart;
+import com.example.barofarm_backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
