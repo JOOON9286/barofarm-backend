055055c29792083f6e7c2f35fe0c630916a2acec
minaaa <tera9131@gmail.com>
2025-05-08 09:10:32 +0900

    변수명 username -> name 변경

--- src/main/java/com/example/barofarm_backend/dto/request/SignupRequest.java
+++ src/main/java/com/example/barofarm_backend/dto/request/SignupRequest.java
@@ -11,7 +11,7 @@ public class SignupRequest {
 
     @NotBlank(message = "아이디는 필수입니다.")
     @Size(min = 3, max = 20, message = "아이디는 3~20자 사이여야 합니다.")
-    private String username;
+    private String name;
 
     @NotBlank(message = "비밀번호는 필수입니다.")
     @Size(min = 4, message = "비밀번호는 최소 4자 이상이어야 합니다.")
