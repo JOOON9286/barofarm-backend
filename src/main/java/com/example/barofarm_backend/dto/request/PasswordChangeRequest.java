9c5b5e6edb33338d7f99137f5d15533f6ddcb532
mark7 <mark7967@kyungmin.ac.kr>
2025-05-15 16:47:38 +0900

    feat: 비밀번호 변경 요청 DTO 생성

---
+++ src/main/java/com/example/barofarm_backend/dto/request/PasswordChangeRequest.java
@@ -0,0 +1,11 @@
+package com.example.barofarm_backend.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PasswordChangeRequest {
+    private String currentPassword;
+    private String newPassword;
+}
