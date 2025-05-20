0b45ca53a23860c68d592c2538f4e4f3003bbd96
minaaa <tera9131@gmail.com>
2025-05-20 19:41:36 +0900

    Add 상품 Entity 추가

--- src/main/java/com/example/barofarm_backend/entity/Product.java
+++ src/main/java/com/example/barofarm_backend/entity/Product.java
@@ -15,9 +15,8 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String name;              // 농산물 이름
+    private Long productId;
+    private String productName;              // 농산물 이름
     private String description;       // 상세 설명
     private int price;                // 가격
     private int stockQuantity;        // 재고 수량
@@ -25,16 +24,6 @@ public class Product {
     private String imageUrl;          // 이미지
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
-
-    @PrePersist
-    protected void onCreate() {
-        createdAt = LocalDateTime.now();
-    }
-
-    @PreUpdate
-    protected void onUpdate() {
-        updatedAt = LocalDateTime.now();
-    }
 
 
 }
