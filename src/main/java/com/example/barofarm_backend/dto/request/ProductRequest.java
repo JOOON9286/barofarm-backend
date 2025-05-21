4b815d47ac1e8c947d1d97af0fe9ef158e0d44c6
minaaa <tera9131@gmail.com>
2025-05-21 20:12:26 +0900

    Add Product Request Dto

--- src/main/java/com/example/barofarm_backend/dto/request/ProductRequest.java
+++ src/main/java/com/example/barofarm_backend/dto/request/ProductRequest.java
@@ -1,5 +1,6 @@
 package com.example.barofarm_backend.dto.request;
 
+import com.example.barofarm_backend.entity.User;
 import lombok.*;
 
 @Getter
@@ -9,14 +10,18 @@ import lombok.*;
 @Builder
 public class ProductRequest {
 
-    private String name;               // 농산물 이름
-    private String description;        // 상세 설명
-    private int price;                 // 가격
-    private int stockQuantity;         // 재고 수량
-    private String category;           // 과일, 채소 등 카테고리
-    private String imageUrl;           // 이미지 URL
-    private String origin;             // 원산지
-    private String salesUnit;          // 판매 단위 (예: 1팩)
-    private String weight;             // 중량 정보 (예: 1~5kg)
+    private Long productId;           // 상품 ID
+    private String productName;              // 농산물 이름
+    private String description;       // 상세 설명
+    private int price;                // 가격
+    private String stockQuantity;        // 재고 수량
+    private String category;          // 과일, 채소 등
+    private String imageUrl;          // 이미지 URL
+    private String origin;            // 원산지
+    private String salesUnit;         // 판매 단위 (예: 1팩)
+    private String weight;            // 중량 정보 (예: 1~5kg)
+    private String createdAt;         // 생성일 (String 형태로 반환)
+    private String updatedAt;         // 수정일 (String 형태로 반환)
+//    private Long userId;    // 등록 회원 id
 
 }
