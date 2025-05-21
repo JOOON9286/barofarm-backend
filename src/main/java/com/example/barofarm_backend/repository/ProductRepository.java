3048b1724dbc85d14547951de90928c3cd458243
minaaa <tera9131@gmail.com>
2025-05-21 11:14:31 +0900

    Modify 미구현 기능 주석처리

--- src/main/java/com/example/barofarm_backend/repository/ProductRepository.java
+++ src/main/java/com/example/barofarm_backend/repository/ProductRepository.java
@@ -9,12 +9,12 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    // 카테고리별 상품 조회
-    List<Product> findByCategory(String category);
-
-    // 이름에 특정 키워드가 포함된 상품 검색
-    List<Product> findByNameContaining(String keyword);
-
-    // 재고가 0보다 큰 상품만 조회
-    List<Product> findByStockQuantityGreaterThan(int quantity);
+//    // 카테고리별 상품 조회
+//    List<Product> findByCategory(String category);
+//
+//    // 이름에 특정 키워드가 포함된 상품 검색
+//    List<Product> findByNameContaining(String keyword);
+//
+//    // 재고가 0보다 큰 상품만 조회
+//    List<Product> findByStockQuantityGreaterThan(int quantity);
 }
