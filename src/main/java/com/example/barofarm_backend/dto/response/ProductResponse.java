4e95dae31c910f5e41da01d581ac41665fe07286
minaaa <tera9131@gmail.com>
2025-05-21 20:12:34 +0900

    Add Product Response Dto

--- src/main/java/com/example/barofarm_backend/dto/response/ProductResponse.java
+++ src/main/java/com/example/barofarm_backend/dto/response/ProductResponse.java
@@ -1,5 +1,6 @@
 package com.example.barofarm_backend.dto.response;
 
+import com.example.barofarm_backend.entity.User;
 import lombok.*;
 
 @Getter
@@ -10,10 +11,10 @@ import lombok.*;
 public class ProductResponse {
 
     private Long productId;           // 상품 ID
-    private String name;              // 농산물 이름
+    private String productName;              // 농산물 이름
     private String description;       // 상세 설명
     private int price;                // 가격
-    private int stockQuantity;        // 재고 수량
+    private String stockQuantity;        // 재고 수량
     private String category;          // 과일, 채소 등
     private String imageUrl;          // 이미지 URL
     private String origin;            // 원산지
@@ -21,4 +22,5 @@ public class ProductResponse {
     private String weight;            // 중량 정보 (예: 1~5kg)
     private String createdAt;         // 생성일 (String 형태로 반환)
     private String updatedAt;         // 수정일 (String 형태로 반환)
+    private Long userId;    // 등록 회원 id
 }
