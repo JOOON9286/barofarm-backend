1dcc62caf06c849dc1ec604734860e058f9355b0
minaaa <tera9131@gmail.com>
2025-05-21 13:48:28 +0900

    Add 상품 request Dto 추가

--- src/main/java/com/example/barofarm_backend/dto/request/ProductRequest.java
+++ src/main/java/com/example/barofarm_backend/dto/request/ProductRequest.java
@@ -9,10 +9,14 @@ import lombok.*;
 @Builder
 public class ProductRequest {
 
-    private String name;
-    private String description;
-    private int price;
-    private int stockQuantity;
-    private String category;
-    private String imageUrl;
+    private String name;               // 농산물 이름
+    private String description;        // 상세 설명
+    private int price;                 // 가격
+    private int stockQuantity;         // 재고 수량
+    private String category;           // 과일, 채소 등 카테고리
+    private String imageUrl;           // 이미지 URL
+    private String origin;             // 원산지
+    private String salesUnit;          // 판매 단위 (예: 1팩)
+    private String weight;             // 중량 정보 (예: 1~5kg)
+
 }
