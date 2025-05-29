9d55c4a94a71d1a1001e0aa090f678549e673747
minaaa <tera9131@gmail.com>
2025-05-29 14:38:00 +0900

    Add method findByUser

--- src/main/java/com/example/barofarm_backend/repository/ProductRepository.java
+++ src/main/java/com/example/barofarm_backend/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.example.barofarm_backend.repository;
 
 import com.example.barofarm_backend.entity.Product;
+import com.example.barofarm_backend.entity.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,6 @@ import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+    List<Product> findByUser(User user);
 }
     
