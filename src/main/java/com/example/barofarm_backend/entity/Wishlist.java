753c618ebb4c3c45afa0d03eb509d272bc0e1ac6
minaaa <tera9131@gmail.com>
2025-06-06 18:20:07 +0900

    Add 찜목록(상품담은목록) 엔티티 추가

---
+++ src/main/java/com/example/barofarm_backend/entity/Wishlist.java
@@ -0,0 +1,30 @@
+package com.example.barofarm_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "wishlist")
+public class Wishlist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    private LocalDateTime likedAt;
+}
