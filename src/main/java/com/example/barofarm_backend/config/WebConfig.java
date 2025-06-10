1a3158406ad2c65d4209509620ad74e421fd5a56
mark7 <mark7967@kyungmin.ac.kr>
2025-06-10 22:53:14 +0900

    리뷰 업로드 설정

--- src/main/java/com/example/barofarm_backend/config/WebConfig.java
+++ src/main/java/com/example/barofarm_backend/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.example.barofarm_backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,11 +11,16 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000","http://localhost:3001")// ✅ 정확한 프론트 주소로
+                .allowedOrigins("http://localhost:3000","http://localhost:3001")//  정확한 프론트 주소로
                 .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Custom-Header")
                 .allowCredentials(true)
                 .maxAge(3600);
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:uploads/");
     }
 }
