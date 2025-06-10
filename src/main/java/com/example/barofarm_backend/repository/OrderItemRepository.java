5b19c028e27e8cd5cfcd2aa3c63cc8f29c134ba9
minaaa <tera9131@gmail.com>
2025-06-10 21:43:35 +0900

    Add 특정 판매자(user_id)가 등록한 상품들에 대한 OrderItem 조회

--- src/main/java/com/example/barofarm_backend/repository/OrderItemRepository.java
+++ src/main/java/com/example/barofarm_backend/repository/OrderItemRepository.java
@@ -2,6 +2,14 @@ package com.example.barofarm_backend.repository;
 
 import com.example.barofarm_backend.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+        // 특정 판매자(user_id)가 등록한 상품들에 대한 OrderItem 조회
+    List<OrderItem> findByProductIdIn(List<Long> productIds);
+
+
 }
