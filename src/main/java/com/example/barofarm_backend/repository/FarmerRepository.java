6c6c0cfee68d48f6be9a01315f0c8efd7fad6992
mark7 <mark7967@kyungmin.ac.kr>
2025-11-26 17:32:08 +0900

    농부 리퍼지토리 추가

--- src/main/java/com/example/barofarm_backend/repository/FarmerRepository.java
+++ src/main/java/com/example/barofarm_backend/repository/FarmerRepository.java
@@ -3,8 +3,9 @@ package com.example.barofarm_backend.repository;
 import com.example.barofarm_backend.entity.Farmer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FarmerRepository extends JpaRepository<Farmer, Long> {
-
+    List<Farmer> findByIsVerified(boolean isVerified);
 }
