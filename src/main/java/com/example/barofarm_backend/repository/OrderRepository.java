07d4cccfcaa43a0f0b12aea5a5c08e00bda76487
mark7 <mark7967@kyungmin.ac.kr>
2025-11-26 17:33:02 +0900

    주문리퍼지토리 추가

--- src/main/java/com/example/barofarm_backend/repository/OrderRepository.java
+++ src/main/java/com/example/barofarm_backend/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package com.example.barofarm_backend.repository;
 
 import com.example.barofarm_backend.entity.Order;
 import com.example.barofarm_backend.entity.User;
+import com.example.barofarm_backend.entity.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +10,5 @@ import java.util.List;
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByUser(User user);
 
+    long countByStatus(OrderStatus status);
 }
