51dd950f09cbde92a8c796d46de6cce9f63b576f
mark7 <mark7967@kyungmin.ac.kr>
2025-11-26 17:27:49 +0900

    관리자 유저  dto 추가

---
+++ src/main/java/com/example/barofarm_backend/admin/dto/AdminUserDto.java
@@ -0,0 +1,14 @@
+// 📁 admin/dto/AdminUserDto.java
+package com.example.barofarm_backend.admin.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AdminUserDto {
+    private Long id;
+    private String username;
+    private String email;
+    private String role;     // "ROLE_USER"
+}
