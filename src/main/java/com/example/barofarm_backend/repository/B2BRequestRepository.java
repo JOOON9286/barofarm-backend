262a7b7ac796b7423c87e4f888773ad50a898a95
mark7 <mark7967@kyungmin.ac.kr>
2025-11-26 17:30:34 +0900

    b2b엔티티 리퍼지토리 수정

--- src/main/java/com/example/barofarm_backend/repository/B2BRequestRepository.java
+++ src/main/java/com/example/barofarm_backend/repository/B2BRequestRepository.java
@@ -1,12 +1,11 @@
-package com.example.barofarm_backend.repository;
+package com.example.barofarm_backend.repository; // ⭐️ 경로가 변경되었습니다.
 
 import com.example.barofarm_backend.entity.B2BRequest;
+import com.example.barofarm_backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
-@Repository
 public interface B2BRequestRepository extends JpaRepository<B2BRequest, Long> {
-//    List<B2BRequest> findByUserId(Long userId);
+    // 특정 회원의 B2B 신청 내역 조회
+    List<B2BRequest> findByUser(User user);
 }
